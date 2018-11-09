46a54bd1416d771853010b6bdfab23dff19a61c7
jv <ljw@live.jp>
2018-11-09 09:22:03 +0800

    build: why-did-you-computed@1.0.0

--- example/main.js
+++ example/main.js
@@ -1,8 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
 
 import test from '@'
 test()
@@ -10,7 +7,5 @@ test()
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
   render: h => h(App)
 }).$mount('#app')
